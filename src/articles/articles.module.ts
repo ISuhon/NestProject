f0a6309fc2ac5e30a196411fccc442df4c60b351
LordMushroom1 <lordmushon1@gmail.com>
2024-12-26 20:10:39 +0200

    articles controller

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
+import { ArticlesController } from './articles.controller';
 
 @Module({
-  providers: [ArticlesService]
+  providers: [ArticlesService],
+  controllers: [ArticlesController]
 })
 export class ArticlesModule {}
